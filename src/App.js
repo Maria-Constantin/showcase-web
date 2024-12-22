b9b12ae031b8f972be53bab69bfa19451f17e590
MC <maria.aurelia.constantin@gmail.com>
2024-12-22 14:12:00 +0000

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import HomePage from './pages/Home';
-import BubbleContainer from './containers/BubbleContainer';
 
 function App() {
   return (
     <div className="App">
-     <HomePage/>
+      <HomePage/>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,17 @@ import React from "react";
 import NavbarComponent from "../components/NavbarComponent";
 import FooterComponent from "../components/FooterComponent";
 import HomeMainContent from "../containers/HomeMainContent";
-import Parallax from "../components/Parralax";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/home.scss";
 
 const HomePage = () => {
   return (
     <>
-      {/* Navigation Bar */}
       <NavbarComponent />
-      {/* Main Content */}
+
       <HomeMainContent />
 
       <FooterComponent />
-
     </>
   );
 };
